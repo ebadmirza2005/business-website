7064da515bff011965f9649dbbd0e464a6a051f6
Ebad Mirza <ebadmirza.2005@gmail.com>
2026-04-11 11:51:25 +0100

    favicon icon change

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Faaz Pro Tech</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="icon" type="image/x-icon" href="assets/logo.png">
+    <link rel="icon" type="image/x-icon" sizes="32x32" href="assets/favicon.png">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
