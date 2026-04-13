25bcd8e48548cd8cb60d4229ef8714d000a0bee2
Ebad Mirza <ebadmirza.2005@gmail.com>
2026-04-13 13:34:04 +0500

    add meta icon for seo

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <title>Faaz Pro Tech</title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/x-icon" href="assets/brain.png">
+    <link rel="icon" type="image/png" href="assets/brain.png">
     <link rel="shortcut icon" href="assets/brain.png">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
