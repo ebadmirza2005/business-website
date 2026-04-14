756e8a8c566a18e13f0addcf86b7f47e9fca6fa7
Ebad Mirza <ebadmirza.2005@gmail.com>
2026-04-14 10:52:06 +0500

    send_request file fixed

--- database.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `bussiness_website`
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
-
-USE `bussiness_website`;
--- smtp_config.php
+++ smtp_config.php
@@ -3,11 +3,11 @@
 return [
     "host" => "smtp.hostinger.com",
     "port" => 587,
-    "username" => "admin@faazprotech.com",
+    "username" => "info@faazprotech.com",
     // Use your Hostinger mailbox password here.
     "password" => "Faazpro@123",
-    "from_email" => "admin@faazprotech.com",
-    "recipient_email" => "admin@faazprotech.com",
+    "from_email" => "info@faazprotech.com",
+    "recipient_email" => "info@faazprotech.com",
     "from_name" => "Faaz Pro Tech",
     "encryption" => "tls",
     "debug" => false,
